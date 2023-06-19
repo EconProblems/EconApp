7a10f83d85da88572a63ab6348ffbc137446b219
Brian Kuzma <78782770+kuzmabr@users.noreply.github.com>
2023-06-19 14:27:50 -0700

    fixed supply three, added dummy questions

--- DummyData/dummyData.js
+++ DummyData/dummyData.js
@@ -59,42 +59,36 @@ const supplyQuestions2 = {
   },
   {
     question: 'What determinant of supply is impacted when new 3D printing technology allows for faster production of plastic baby toys?',
-    pos:'capital',
     wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
     wordAns: ['capital'],
     ans:'New technology means an increase in supply. The cost of production will likely fall.'
   },
   {
     question: 'There is an influx of factory workers after an immigration law is passed encouraging skilled workers to immigrate',
-    pos:'capital',
     wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
     wordAns: ['labor'],
     ans:'Supply increases with an increase in people willing and able to do produce a good.'
   },
   {
     question: 'What will happen to the supply of cars if there is a shortage of skilled automotive technicians?',
-    pos: '',
     wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
     wordAns: ['labor'],
     ans: 'The supply of cars will decrease due to a shortage of skilled workers in the automotive industry.'
   },
   {
     question: 'What determinant of supply is impacted when a new government regulation imposes stricter environmental standards?',
-    pos: '',
     wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
     wordAns: ['entrepreneurship'],
     ans: 'Stricter environmental standards affect the willingness and ability of entrepreneurs to produce, thus impacting the supply.'
   },
   {
     question: 'How does an increase in the cost of machinery and equipment impact the supply of goods?',
-    pos: '',
     wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
     wordAns: ['capital'],
     ans: 'Higher costs of machinery and equipment reduce the supply of goods.'
   },
   {
     question: 'What will happen to the supply of smartphones if there is a decrease in the availability of rare earth metals?',
-    pos: '',
     wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
     wordAns: ['land'],
     ans: 'The supply of smartphones will decrease due to a decrease in the availability of essential raw materials.'
@@ -109,44 +103,42 @@ const supplyQuestions3 = {
       question: 'What will happen to the supply of oranges if farmers decide to repurpose their fields for tangelos',
       pos: 'left'
     },
-    // {
-    //   question: 'How will the supply of wheat be affected if a new government subsidy is introduced for wheat farmers?',
-    //   pos: 'right'
-    // },
-    // {
-    //   question: 'What will happen to the supply of dog leashes if the government taxes nylon (the stuff leashes are made of)?',
-    //   pos: 'left'
-    // },
-    // {
-    //   question: 'What will be the impact on the supply of toys if the cost of plastic, a key raw material, significantly increases?',
-    //   pos: 'left'
-    // },
-    // {
-    //   question: 'What will happen to the global coffee supply if a major coffee-producing country faces a severe drought?',
-    //   pos: 'left'
-    // },
-    // {
-    //   question: 'How will the supply of gasoline be affected if a new law mandates higher ethanol (more expensive) content in fuel?',
-    //   pos: 'left'
-    // },
-    // {
-    //   question: 'If there is a sudden surge in demand for bread in town due to a population increase, what will happen to the supply of bread if bakeries are operating at maximum capacity?',
-    //   pos: 'right'
-    // },
-    // {
-    //   question: 'What will be the impact on the supply of shirts if there is a shortage of cotton due to extreme weather conditions?',
-    //   pos: 'right'
-    // },
+    {
+      question: 'How will the supply of wheat be affected if a new government subsidy is introduced for wheat farmers?',
+      pos: 'right'
+    },
+    {
+      question: 'What will happen to the supply of dog leashes if the government taxes nylon (the stuff leashes are made of)?',
+      pos: 'left'
+    },
+    {
+      question: 'What will be the impact on the supply of toys if the cost of plastic, a key raw material, significantly increases?',
+      pos: 'left'
+    },
+    {
+      question: 'What will happen to the global coffee supply if a major coffee-producing country faces a severe drought?',
+      pos: 'left'
+    },
+    {
+      question: 'How will the supply of gasoline be affected if a new law mandates higher ethanol (more expensive) content in fuel?',
+      pos: 'left'
+    },
+    {
+      question: 'If there is a sudden surge in demand for bread in town due to a population increase, what will happen to the supply of bread if there are some bakeries currently "shuttered" and not operating',
+      pos: 'right'
+    },
+    {
+      question: 'What will be the impact on the supply of shirts if there is a shortage of cotton due to extreme weather conditions?',
+      pos: 'left'
+    },
     {
       question: 'Select the determinant of supply impacted in the pearl diving industry when divers leave to become ship wreck explorers.',
-      pos: '',
       wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
       wordAns: ['labor'],
       ans: 'Laborers are needed to harvest the oysters pearls grow in.'
     },
     {
       question: 'What determinant of supply is in focus when management decides to change priorities of a company',
-      pos: '',
       wordBank: ['land', 'labor', 'capital', 'entrepreneurship'],
       wordAns: ['entrepreneurship'],
       ans: 'This is the decision making process of what, how, and how much to make of a good or service.'
---
+++ DummyData/supplyDemandDummyData.js
@@ -0,0 +1,37 @@
+const supplyDemandQuestionsFunny = [
+  {
+    question: "What will happen in the pizza market if new studies determine that it's the best cure for Monday blues?",
+    pos: 'right',
+    curve: 'demand'
+  },
+  {
+    question: 'What will happen in the ice cream market if all of the cows walk off the job and go on strike?',
+    pos: 'left',
+    curve: 'supply'
+  },
+  {
+    question: 'What will happen within the pencil sharpener market if the magazines declare them the hottest fashion accessory?',
+    pos: 'right',
+    curve: 'demand'
+  },
+  {
+    question: 'What will happen to the tennnis ball market if the main factory is flooded in a Hurricane?',
+    pos: 'left',
+    curve: 'supply'
+  },
+  {
+    question: 'What will happen with hoverboards if the price of sneakers that make you fly drops by 99%?',
+    pos: 'left',
+    curve: 'demand'
+  },
+  {
+    question: 'What will happen to Hotel rooms if Taylor Swift comes to town?',
+    pos: 'right',
+    curve: 'demand'
+  },
+  {
+    question: 'What will happen to the Turkey market if cauliflower becomes the main course for meals at Thanksgiving?',
+    pos: 'left',
+    curve: 'demand'
+  },
+];
